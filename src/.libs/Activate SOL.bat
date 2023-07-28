ad51dd6e8a31100c526e17ff4ab90d4d598e822d
zhoubin <zhouBin@aa.com>
2023-07-28 16:03:58 +0800

    Activate cmd example

--- src/.libs/Activate SOL.bat
+++ src/.libs/Activate SOL.bat
@@ -1,3 +1,3 @@
 
-.\ipmitool.exe -H 192.168.0.100 -U admin -P admin -I lanp -p 623 -b 0 sol activate
+.\ipmitool.exe -H 192.168.0.100 -U admin -P admin -I lanp -p 623 sol activate instance=1
 .\ipmitool.exe -H 192.168.0.100 -U admin -P admin -I lanp -p 623  sol deactivate
